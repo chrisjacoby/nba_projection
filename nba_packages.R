2a85353ef1f8666c0160c50b2200827d7a35c983
chrisjacoby <chr1sjac0by@yahoo.com>
2018-09-03 14:57:18 -0700

    commit.1

---
+++ nba_packages.R
@@ -0,0 +1,5 @@
+# NBA Stat Package for R
+# https://github.com/abresler/nbastatR
+# https://www.rdocumentation.org/packages/nbastatR/versions/0.1.03
+devtools::install_github("abresler/nbastatR")
+library("nbastatR") # note requires a bunch of other packages which are listed in the import
